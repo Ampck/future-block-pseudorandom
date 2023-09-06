7ed7297a30966178e24d3a5da5a9a920a66cf2ba
ampck <a1j2m4@gmail.com>
2023-09-06 01:10:43 -0400

    created and implemented basic random number generator in solidity

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const RG = await ethers.getContractFactory("RandomGenerator")
+  const rg = await RG.deploy()
+  await rg.deployed();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(rg.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
