6a047baa5dfe1d7ccbffaad9d4f89f9b5d6e2578
ampck <a1j2m4@gmail.com>
2023-09-14 02:08:32 -0400

    created and set up CoinFlip.sol

---
+++ contracts/CoinFlip.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+import "./Random.sol";
+
+contract CoinFlip is Random {
+	
+}
